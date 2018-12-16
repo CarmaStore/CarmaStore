d06d8ef38305f37aa0a8b57f8ee6d8a8582ed100
Hot2Touch <manuelxcostales8@gmail.com>
2018-12-16 16:15:52 -0500

    test

---
+++ web/index/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { ListgamesComponent } from './component/listgames/listgames.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    ListgamesComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ web/index/src/app/component/listgames/listgames.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-listgames',
+  templateUrl: './listgames.component.html',
+  styleUrls: ['./listgames.component.css']
+})
+export class ListgamesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
